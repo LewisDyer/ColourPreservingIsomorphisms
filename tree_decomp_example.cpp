29cca1635f44fde50a906e8ba591c1a9cd7d4a08
LewisDyer <lewisd251@gmail.com>
2022-10-17 17:20:14 +0100

    Test program for tree decompositions

--- tree_decomp_example.cpp
+++ tree_decomp_example.cpp
@@ -10,6 +10,7 @@
 #include "nice_tree_decomposition.hpp"
 #include <iostream>
 #include <boost/graph/graphviz.hpp>
+#include <boost/graph/adjacency_list.hpp>
 
 using namespace boost;
 using Graph = adjacency_list<listS, vecS, undirectedS, property<vertex_name_t, int, property<vertex_index_t, size_t>>, property<edge_index_t, int>>;
@@ -22,14 +23,38 @@ using Decomposition_bags = associative_property_map<Decomposition_bags_map>;
 int main() {
     Graph path = Graph(10);
 
-    for(int i=0; i < 10; i++) {
+    for(int i=0; i < 8; i++) {
         add_edge(i, i+1, path);
     }
 
-    std::ofstream path_graph;
-    path_graph.open("path.dot");
+    // std::ofstream path_graph;
+    // path_graph.open("path.dot");
+    // write_graphviz(path_graph, path);
+    // path_graph.close();
 
-    boost::write_graphviz(path_graph, path);
-    path_graph.close();
+    Decomposition_bags_map bm;
+    Decomposition_bags b(bm);
+
+    Graph dec_path;
+
+    bool x = tree_decomposition(path, dec_path, b, (-0.75));
+
+    std::cout << "this has been updated\n";
+
+    for(int i=0; i < 10; i++) {
+        std::cout << "{";
+        std::set<Graph_vertex> current_bag = get(b,i);
+        for (auto const &item: current_bag) {
+            std::cout << item << " ";
+        }
+        std::cout << "}\n";
+    }
+
+    std::ofstream path_dec;
+    path_dec.open("path_dec.dot");
+    boost::write_graphviz(path_dec, dec_path);
+    path_dec.close();
+
+
 }
 
