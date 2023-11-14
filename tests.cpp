ffd218fecf0a8811f0455ccfffcd2b4215f97eb0
LewisDyer <lewisd251@gmail.com>
2023-11-14 14:32:35 +0000

    Try to get CPI test working, removing quick_tour

--- tests.cpp
+++ tests.cpp
@@ -1,6 +1,8 @@
 #define BOOST_TEST_MODULE CPITests
 #include <boost/test/included/unit_test.hpp>
 #include "graph_generators.h"
+#include "graph_utils.h"
+#include "count_colourful_isomorphisms.h"
 
 using Graph = adjacency_list<listS, vecS, undirectedS, property<vertex_name_t, int, property<vertex_index_t, size_t>>, property<edge_index_t, int>>;
 
@@ -61,4 +63,26 @@ BOOST_AUTO_TEST_CASE(clique_100_has_4950_edges) {
     Graph P = clique(100);
 
     BOOST_CHECK_EQUAL(boost::num_edges(P),4950);
+}
+
+BOOST_AUTO_TEST_CASE(single_vertex_found_in_path_1000) {
+    DiGraph T = makeRooted(clique(1), 0);
+
+    Colours col_T;
+
+    col_T[0] = 0;
+
+    ColourMap colour_T(col_T);
+
+    Graph G = path(1000);
+
+    Colours col_G;
+
+    for(int i=0; i < boost::num_vertices(G); i++) {
+        col_G[i] = 0;
+    }
+
+    ColourMap colour_G(col_G);
+
+    BOOST_CHECK_EQUAL(new_tree_count(T, 1, colour_T, G, colour_G), 1000);
 }
